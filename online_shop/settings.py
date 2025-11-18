eb21c1455c09a9fee1a0013dd5be37163818a4be
Luka929 <Luka929@example.com>
2025-11-18 13:54:29 -0500

    final changes

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -81,13 +81,11 @@ WSGI_APPLICATION = 'online_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
